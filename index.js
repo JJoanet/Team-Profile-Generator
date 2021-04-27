4488df7c2c37abea7d48109c8d4822c2d77ea984
JeremyJ <JeremyJoanet@protonmail.com>
2021-04-26 23:19:28 -0500

    added profile functionality and test html file, still need to add test files for completion

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateHtml = require('./lib/generatehtml');
 const createManager = require('./lib/manager');
+const createEngineer = require('./lib/engineer');
+const createIntern = require('./lib/intern')
 
+const team = new Array;
+let pagenameResult;
 
 const pageName = {
     type: 'input',
@@ -48,6 +52,18 @@ const managerQuestions = [{
     message: 'What is this team members office number?'
 }];
 
+const engineerQuestions = [{
+    type: 'input',
+    name: 'github',
+    message: `What is the engineer's Github username?`
+}];
+
+const internQuestions = [{
+    type: 'input',
+    name: 'school',
+    message: `What is the intern's school?`
+}];
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (error) => 
     error ? console.error(error) : console.log('success!')
@@ -57,10 +73,12 @@ function init() {
     inquirer
         .prompt(pageName)
         .then((pageName) => {
-            writeToFile('index.html', generateHtml(pageName));
+            pagenameResult = pageName;
             teamAdd();
+            return pagenameResult;
         });
 }
+
 
 function teamAdd() {
     inquirer
@@ -69,40 +87,54 @@ function teamAdd() {
             if(newMember.newMember === 'Yes'){
                 inquirer
                     .prompt(questions)
-                    .then((questions) => {
-                        roleSelected(questions);
-                        return questions;
+                    .then((questionResponse) => {
+                        return roleSelected(questionResponse);
                     });
+            } else if (newMember.newMember === "No") {
+                let packagedData = {
+                    title: pagenameResult,
+                    team: team
+                }
+                writeToFile('./dist/index.html', generateHtml(packagedData));
             }
         })
 }
 
-function roleSelected() {
+function roleSelected(passData) {
     inquirer
         .prompt(role)
         .then((role) => {
             if(role.role == 'Manager'){
                 inquirer
                     .prompt(managerQuestions)
-                    .then((questions, managerQuestions) => {
-                        createManager(questions, managerQuestions);
-                        teamAdd();
-
+                    .then((managerQuestions) => {
+                        createManager(passData, managerQuestions);
+                        const newManager = createManager(passData, managerQuestions)
+                        team.push(newManager);
+                        return teamAdd();
                     });
             } else if (role.role == 'Intern'){
                 inquirer
-                    .prompt(managerQuestions)
+                    .prompt(internQuestions)
                     .then((internQuestions) => {
-                        teamAdd();
+                        createIntern(passData, internQuestions);
+                        const newIntern = createIntern(passData, internQuestions)
+                        team.push(newIntern);
+                        return teamAdd();
                     });
             } else if (role.role == 'Engineer') {
                 inquirer
-                    .prompt(managerQuestions)
+                    .prompt(engineerQuestions)
                     .then((engineerQuestions) => {
-                        teamAdd();
+                        createEngineer(passData, engineerQuestions);
+                        const newEngineer = createEngineer(passData, engineerQuestions)
+                        team.push(newEngineer);
+                        return teamAdd();
                     });
             }
         })
 };
+
+
 
 init();
