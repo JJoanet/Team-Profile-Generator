01cac9e5c94c2042207dae5e3b31317142b7ab21
JeremyJ <JeremyJoanet@protonmail.com>
2021-04-30 00:37:39 -0500

    Update intern.test.js

--- test/intern.test.js
+++ test/intern.test.js
@@ -4,12 +4,12 @@ describe('Intern', () => {
     describe('Create', () => {
         it('Should create a add Intern role and github username', () => {
             
-            const result = new Intern.Intern("Matt Reisdorf", "01", "mreisdorf9717@gmail.com", "App State", "Intern");
+            const result = new Intern.Intern("John Doe", "01", "JDoe@gmail.com", "App State", "Intern");
 
             expect(result).toEqual({
-                employeeName: "Matt Reisdorf", 
+                employeeName: "John Doe", 
                 id: "01",
-                email: "mreisdorf9717@gmail.com",
+                email: "JDoe@gmail.com",
                 school: "App State",
                 role: "Intern"
             });
